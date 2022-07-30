d90e41dae1d5f6391ffd23de134195240a24d8d8
freddd1 <itamar.fradkin@gmail.com>
2022-07-30 16:15:04 +0300

    added `SentencesDataset`

--- dataset.py
+++ dataset.py
@@ -1,6 +1,22 @@
 from torch.utils.data import Dataset
 import transformers
-from typing import List
+from typing import List, Tuple
+
+
+class SentencesDataset(Dataset):
+    def __init__(self, sentences1: List[str], sentences2: List[str], labels: List[int]):
+        self.labels = labels
+        self.sentences1 = sentences1
+        self.sentences2 = sentences2
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx) -> dict:
+        s1 = self.sentences1[idx]
+        s2 = self.sentences2[idx]
+        label = self.labels[idx]
+        return dict(sentences1=s1, sentences2=s2, labels=label)
 
 
 class TokenizedDataset(Dataset):
